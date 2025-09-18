b8ca8608ba9c8cd69aceb8efeac11d7e76b412f3
ValentineAnstett <valentine.anstett@gmailcom>
2025-09-18 09:53:46 +0200

    Analyse du sol : nuage de points et nmds avec ellipses

--- 12_explosol.R
+++ 12_explosol.R
@@ -257,3 +257,204 @@ for (var in variables) {
     }
   }
 }
+
+####NUAGE DE POINTS ####
+
+Compo = colnames(Sol)[!(colnames(Sol) %in% c("Annee", "Site", "ID_LAG"))]
+
+for (comp in Compo) {
+  df_wide = Sol %>%
+    select(Annee, Site, ID_LAG, value = all_of(comp)) %>%
+    filter(Annee %in% c(2021, 2025)) %>%
+    pivot_wider(
+      names_from = Annee,
+      values_from = value,
+      names_prefix = paste0(comp, "_")
+    ) %>%
+    drop_na()  # enlève les lignes incomplètes
+  col_2021 = paste0(comp, "_2021")
+  col_2025 = paste0(comp, "_2025")
+  
+  if (all(c(col_2021, col_2025) %in% colnames(df_wide))) {
+    
+    p = ggplot(df_wide, aes_string(x = col_2025, y = col_2021, color = "Site")) +
+      geom_jitter(size = 3,width = 0.2, height = 0.2) +
+      geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red", linewidth = 1) +
+      coord_fixed(ratio = 1, xlim = c(0, 100), ylim = c(0, 100), expand = TRUE) +
+      labs(
+        title = paste("Niveau de", comp),
+        x = "2025",
+        y = "2020",
+        color = "Site"
+      ) +
+      theme_minimal()+
+      theme(
+        panel.grid.major = element_line(color = "grey60", size = 0.6),  # Quadrillage principal renforcé
+        panel.grid.minor = element_line(color = "grey80", size = 0.3),  # Quadrillage secondaire visible
+        panel.border = element_rect(color = "black", fill = NA, size = 0.8),  # Contour du graphique
+        axis.line = element_line(color = "black", size = 0.8)  # Axes visibles
+      )
+    
+    print(p) 
+  }
+}
+
+
+#### ANALYSE MULTI ####
+
+Data_multi = Sol %>% select(-PARTICULES_FINES)
+sites_a_exclure = c("BPA_PIS", "BAG_GRA", "CAN_NAZ", "HYE_VIE", "THA_SET")
+Data_multi = Data_multi %>%
+  filter(!(Site %in% sites_a_exclure))
+Data_multi$Annee = as.factor(Data_multi$Annee)
+Data_multi$Site = as.factor(Data_multi$Site)
+Data_multi$ID_LAG = as.factor(Data_multi$ID_LAG)
+df_composes = Data_multi %>% select(4:13)
+df_meta = Data_multi %>% select(Annee, Site, ID_LAG)
+df_composes_clean = df_composes %>%
+  mutate(across(everything(), as.numeric)) %>%
+  select(where(~ sd(., na.rm = TRUE) > 0)) %>%
+  filter(rowSums(is.na(.)) < ncol(.)) %>%
+  filter(rowSums(., na.rm = TRUE) > 0)
+
+nmds = metaMDS(df_composes_clean, distance = "bray", k = 2, trymax = 100)
+scores_df = as.data.frame(scores(nmds, display = "sites"))
+scores_df = bind_cols(df_meta, scores_df)
+
+lagunes_2021_2025 = scores_df %>%
+  filter(Annee %in% c(2021, 2025)) %>%
+  group_by(ID_LAG) %>%
+  summarise(annees_presente = list(unique(Annee))) %>%
+  filter(all(c(2021, 2025) %in% unlist(annees_presente))) %>%
+  pull(ID_LAG)
+
+scores_df_filtre = scores_df %>%
+  filter(ID_LAG %in% lagunes_2021_2025)
+
+compo_scores = as.data.frame(scores(nmds, display = "species"))
+compo_scores$Compo = rownames(compo_scores)
+colnames(compo_scores)[1:2] = c("NMDS1", "NMDS2")
+
+# Graph 1 : Ellipses par site
+ggplot(scores_df, aes(x = NMDS1, y = NMDS2, color = Site)) +
+  geom_point(size = 1.5) +
+  stat_ellipse(type = "t", level = 0.68, linewidth = 1) +
+  geom_segment(data = compo_scores,
+               aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2),
+               arrow = arrow(length = unit(0.3, "cm")),
+               color = "black", inherit.aes = FALSE) +
+  geom_text(data = compo_scores,
+            aes(x = NMDS1, y = NMDS2, label = Compo),
+            color = "black", vjust = -0.5, size = 3, inherit.aes = FALSE) +
+  geom_hline(yintercept = 0, color = "black", linewidth = 1, linetype = "dashed") +
+  geom_vline(xintercept = 0, color = "black", linewidth = 1, linetype = "dashed") +
+  
+  theme_minimal() +
+  labs(
+    title = "NMDS des compositions du sol par Site",
+    x = "NMDS1",
+    y = "NMDS2"
+  ) +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+    axis.line = element_line(color = "black", linewidth = 1),
+    panel.grid.major = element_line(color = "grey80", size = 0.5),
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12)
+  )
+
+# Graph 2 : Ellipses par années
+scores_df_filtre_ellipses = scores_df_filtre %>%
+  group_by(Site) %>%
+  filter(n() >= 3) %>%
+  ungroup()
+
+ggplot(scores_df_filtre, aes(x = NMDS1, y = NMDS2, color = Annee)) +
+  geom_point(size = 2) +
+  stat_ellipse(type = "t") +
+  geom_segment(data = compo_scores,
+               aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2),
+               arrow = arrow(length = unit(0.2, "cm")),
+               color = "black", inherit.aes = FALSE) +
+  geom_text(data = compo_scores,
+            aes(x = NMDS1, y = NMDS2, label = Compo),
+            color = "black", vjust = -0.5, inherit.aes = FALSE) +
+  theme_minimal() +
+  labs(title = "NMDS + Ellipses par Année",
+       x = "NMDS1",
+       y = "NMDS2") +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+    axis.line = element_line(color = "black", linewidth = 1),
+    panel.grid.major = element_line(color = "grey80", size = 0.5),
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12)
+  )
+
+# Graph 3 : Ellipses par Site/Année + flèches de trajectoire
+scores_df_filtre = scores_df_filtre %>%
+  mutate(Site_Annee = paste(Site, Annee, sep = "_"))
+
+# Supprimer les groupes avec < 3 points
+scores_df_filtre_ellipse <- scores_df_filtre %>%
+  group_by(Site_Annee) %>%
+  filter(n() >= 3) %>%
+  ungroup()
+
+# Calcul des centroïdes
+centroids <- scores_df_filtre_ellipse %>%
+  group_by(Site, Annee, Site_Annee) %>%
+  summarise(
+    NMDS1 = mean(NMDS1, na.rm = TRUE),
+    NMDS2 = mean(NMDS2, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  arrange(Site, Annee)
+
+# Création des flèches de trajectoire
+arrows_df = centroids %>%
+  group_by(Site) %>%
+  arrange(Annee) %>%
+  mutate(
+    NMDS1_end = lead(NMDS1),
+    NMDS2_end = lead(NMDS2)
+  ) %>%
+  filter(!is.na(NMDS1_end) & !is.na(NMDS2_end)) %>%
+  ungroup()
+
+ggplot(scores_df_filtre_ellipse, aes(x = NMDS1, y = NMDS2, color = Site)) +
+  geom_point(aes(shape = Annee), size = 2) +
+  stat_ellipse(aes(group = Site_Annee), type = "t") +
+  geom_segment(data = arrows_df,
+               aes(x = NMDS1, y = NMDS2, xend = NMDS1_end, yend = NMDS2_end, color = Site),
+               arrow = arrow(type = "closed", length = unit(0.15, "inches")),
+               linewidth = 1) +
+  theme_minimal() +
+  labs(title = "Trajectoire temporelle des Sites dans l’espace NMDS")
+
+
+# Graph 4 : Trajectoires par lagunes (coloration par année)
+ggplot(scores_df_filtre, aes(x = NMDS1, y = NMDS2)) +
+  geom_path(aes(group = ID_LAG),
+            arrow = arrow(type = "closed", length = unit(0.15, "inches")),
+            color = "grey50", linewidth = 0.8) +
+  geom_point(aes(color = Annee), size = 3) +
+  theme_minimal() +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+    axis.line = element_line(color = "black", linewidth = 1),
+    panel.grid.major = element_line(color = "grey80", size = 0.5),
+    axis.title = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12))+ 
+  geom_hline(yintercept = 0, color = "black", linewidth = 0.8, linetype = "dashed") +
+  geom_vline(xintercept = 0, color = "black", linewidth = 0.8, linetype = "dashed")
+
+
+
+
+
+
+
+
+
+
