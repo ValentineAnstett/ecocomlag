d9dc281955c4efa6cb66e6853bc79babf9b82a95
ValentineAnstett <valentine.anstett@gmailcom>
2025-09-24 16:44:47 +0200

    Script pour formatage de dataset

---
+++ 02_Data.R
@@ -0,0 +1,7 @@
+####Data eau 
+
+getwd()
+setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Hydro/Data/Processed")
+Hydro = read.csv("hydro total 2019-2024.csv", header = TRUE, sep = ",", dec=",")
+
+
