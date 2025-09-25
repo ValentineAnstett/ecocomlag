90163af456fb152ae9455d2dbf76a249eef79cf9
ValentineAnstett <valentine.anstett@gmailcom>
2025-09-25 10:12:46 +0200

    Création dataframe pour données uniques par années

--- 02_Data.R
+++ 02_Data.R
@@ -1,7 +1,48 @@
-####Data eau 
-
+####DATA HYDRO####
 getwd()
 setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Hydro/Data/Processed")
-Hydro = read.csv("hydro total 2019-2024.csv", header = TRUE, sep = ",", dec=",")
+Hydro = read.csv("Hydro_Finale_19_23_25.csv", header = TRUE, sep = ",", dec=",")
 
+Hydro = Hydro %>%
+  mutate(date_releve = as.Date(date_releve, format = "%Y-%m-%d"))
 
+##Faire data frame avec valeur unique pour chaque année
+
+#Valeur du mois de germination 
+
+Hydro_mois = Hydro %>%
+  mutate(
+    annee = year(date_releve),
+    mois = month(date_releve)
+  ) %>%
+  filter(
+    annee %in% c(2019,2020, 2025),
+    mois %in% c(4, 5, 6)  # avril, mai, juin
+  )
+# Nombre de sites uniques par année et mois
+site_counts = Hydro_mois %>%
+  group_by(annee, mois) %>%
+  summarise(nb_sites = n_distinct(site), .groups = "drop")
+print(site_counts)
+
+# Trouver le mois qui a le plus de sites pour chaque année
+mois_max_sites = site_counts %>%
+  group_by(annee) %>%
+  filter(nb_sites == max(nb_sites)) %>%
+  ungroup()
+
+print("Mois optimal par année :")
+print(mois_max_sites)
+
+#Creer le dataframe pour germi
+Hydro_germi = Hydro %>%
+  mutate(
+    annee = year(date_releve),
+    mois = month(date_releve)
+  ) %>%
+  filter(
+    annee %in% c(2020, 2025),
+    mois == 4
+  )
+# Importer dans processed 
+write.csv(Hydro_germi, file = "/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_hydro/Hydro_germi.csv", row.names = FALSE)
