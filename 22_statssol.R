547e9995c47c9f0b100b2ff16ff747dc9396a252
ValentineAnstett <valentine.anstett@gmailcom>
2025-09-29 14:26:20 +0200

    màj

--- 22_statssol.R
+++ 22_statssol.R
@@ -20,27 +20,19 @@ sites_exclus = c("BAG_GRA", "HYERES", "VILLEROY")
 
 
 faire_permanova = function(data, colonnes_compo, facteurs, sites_a_exclure = NULL) {
-  # Exclure sites si besoin
   data = data %>%
     filter(!(Site %in% sites_a_exclure)) %>%
     mutate(across(all_of(facteurs), as.factor))  # S'assurer que les facteurs sont bien en facteur
-  
-  # Sélection des variables de composition
   df_compo = data %>%
     select(all_of(colonnes_compo)) %>%
     mutate(across(everything(), as.numeric)) %>%
     select(where(~ sd(., na.rm = TRUE) > 0)) %>%
     filter(rowSums(is.na(.)) < ncol(.))
-  
-  # Métadonnées pour le design
   df_meta = data %>% 
     filter(rowSums(is.na(select(., all_of(colonnes_compo)))) < ncol(.)) %>%
     select(all_of(facteurs))
-  
-  # Calcul des distances (par exemple, euclidiennes ou Bray-Curtis selon le cas)
   distance_matrix = vegdist(df_compo, method = "euclidean")  # ou "bray"
   
-  # PERMANOVA avec adonis2
   formule = as.formula(paste("distance_matrix ~", paste(facteurs, collapse = " + ")))
   
   permanova_result = adonis2(formule, data = df_meta, permutations = 999)
@@ -68,3 +60,5 @@ resultats_permanova_sol = faire_permanova(
 )
 
 print(resultats_permanova_sol)
+
+
