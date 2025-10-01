5fca65fe30c6429314e9086847dbda857320433e
ValentineAnstett <valentine.anstett@gmailcom>
2025-10-01 10:47:42 +0200

    Dataset hydro mensuelle

--- 02_Data.R
+++ 02_Data.R
@@ -1,4 +1,4 @@
-####DATA HYDRO####
+####DATA HYDRO GERMI####
 getwd()
 setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_hydro")
 Hydro = read.csv("Hydro_Finale_19_23_25.csv", header = TRUE, sep = ",", dec=",")
@@ -71,3 +71,47 @@ Hydro_germi = Hydro_germi %>%
 
 # Importer dans processed 
 write.csv(Hydro_germi, file = "/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_hydro/Hydro_germi.csv", row.names = FALSE)
+
+
+
+####DATA HYDRO MENSUELLE####
+getwd()
+setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_hydro")
+Hydro = read.csv("Hydro_Finale_19_23_25.csv", header = TRUE, sep = ",", dec=",")
+
+Hydro = Hydro %>%
+  mutate(date_releve = as.Date(date_releve, format = "%Y-%m-%d"))
+
+
+#Ne garder que les sites suivis mensuellement en 2020+2023+2025
+codes_a_garder = c(
+  # SIG_GSA
+  "SIG_GSA_01", "SIG_GSA_04", "SIG_GSA_06", "SIG_GSA_07", "SIG_GSA_08", "SIG_GSA_09", "SIG_GSA_10",
+  
+  # ORB_ORP
+  "ORB_ORP_01", "ORB_ORP_03", "ORB_ORP_05", "ORB_ORP_06", "ORB_ORP_08",
+  
+  # BAG_PET
+  "BAG_PET_01", "BAG_PET_02", "BAG_PET_03", "BAG_PET_04", "BAG_PET_05", "BAG_PET_06", "BAG_PET_07", "BAG_PET_08", "BAG_PET_11",
+  
+  # EOR_MOT
+  "EOR_MOT_04", "EOR_MOT_05", "EOR_MOT_06", "EOR_MOT_07", "EOR_MOT_08", "EOR_MOT_10",
+  
+  # GCA_RNC
+  "GCA_RNC_01", "GCA_RNC_03", "GCA_RNC_06", "GCA_RNC_07", "GCA_RNC_08", "GCA_RNC_09", "GCA_RNC_10",
+  
+  # FOS_REL
+  "FOS_REL_05", "FOS_REL_07", "FOS_REL_08", "FOS_REL_09", "FOS_REL_10"
+)
+
+Hydro_mens = Hydro[Hydro$code %in% codes_a_garder, ]
+
+#Format date (retirer le jour)
+Hydro_mens$date_releve = as.Date(format(Hydro_mens$date_releve, "%Y-%m-01"))
+Hydro_mens = Hydro_mens[,-c(3,7,13)]
+
+# Importer dans processed 
+write.csv(Hydro_mens, file = "/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_hydro/Hydro_mens.csv", row.names = FALSE)
+
+
+
