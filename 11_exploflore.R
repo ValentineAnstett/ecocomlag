90d09652fa5a9b8d168c89e9b42da47f594257d3
ValentineAnstett <valentine.anstett@gmailcom>
2025-10-10 14:21:23 +0200

    Exclusion des outliers et graph de distributions des espèces par site

--- 11_exploflore.R
+++ 11_exploflore.R
@@ -2,13 +2,180 @@
 
 #Import dataset
 getwd()
-setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Macrophytes_/Data/Processed")
-Cov_tot_moy = read.csv("Macro_CovTot_moy_2025.csv", header = TRUE, sep = ",", dec=".")
+setwd("/home/anstett/Documents/LTM-Flora/Analyses_stats/Analyse_Globale/Data/Processed_Macro")
+Macro_Ptscontacts= read.csv("Macro_Ptscontacts.csv", header = TRUE, sep = ",", dec=".")
 
-Cov_tot_moy = Cov_tot_moy[,-21]
 
-Cov_tot_moy = Cov_tot_moy %>%
-  mutate(across(4:20, as.numeric))
+
+###  Boxplots 2020 vs 2025 ----
+couleurs_annee <- c("2020" = "#F8766D",  # rouge clair
+                    "2025" = "#00BFC4") 
+#### Boxplot sur le total de chaque lagune ----
+ggplot(Macro_Ptscontacts, aes(x = factor(annee), y = TOT, fill = factor(annee))) +
+  geom_boxplot(alpha = 0.5, color = "black") +   # boxplot avec contour noir
+  geom_line(aes(group = LAGUNE), color = "grey50", alpha = 0.7, show.legend = FALSE) +
+  geom_point(color = "black", size = 2, show.legend = FALSE) +
+  scale_fill_manual(values = couleurs_annee) +  # appliquer les couleurs
+  labs(x = "Année", y =  "Valeurs TOT", fill = "Année") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+#Boxplot sur la moyenne des indices par lagunes 
+Macro_Ptscontacts_long = Macro_Ptscontacts %>%
+  filter(annee %in% c(2020, 2025)) %>%
+  pivot_longer(
+    cols = -c(annee, Site, LAGUNE),
+    names_to = "Espece",
+    values_to = "indice"
+  )
+
+df_agg = Macro_Ptscontacts_long %>%
+  group_by(annee, LAGUNE) %>%
+  summarise(moyenne_indice = mean(indice, na.rm = TRUE)) %>%
+  ungroup()
+
+ggplot(df_agg, aes(x = factor(annee), y = moyenne_indice, fill = factor(annee))) +
+  geom_boxplot(alpha = 0.5, color = "black") +   # boxplot avec contour noir
+  geom_line(aes(group = LAGUNE), color = "grey50", alpha = 0.7, show.legend = FALSE) +
+  geom_point(color = "black", size = 2, show.legend = FALSE) +
+  scale_fill_manual(values = couleurs_annee) +  # appliquer les couleurs
+  labs(x = "Année", y = "Moyenne indice par lagune", fill = "Année") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+#### Nombre d'espèces par sites ----
+
+Macro_Ptscontacts = Macro_Ptscontacts %>%
+  rowwise() %>%
+  mutate(n_sp = sum(c_across(-c(annee, Site, LAGUNE)) > 0, na.rm = TRUE)) %>%
+  ungroup()
+
+df_n_sp = Macro_Ptscontacts %>%
+  select(annee,Site, LAGUNE, n_sp) %>%
+  filter(annee %in% c(2020, 2025)) %>%
+  pivot_wider(
+    names_from = annee,
+    values_from = n_sp,
+    names_prefix = "n_sp_"
+  ) %>%
+  drop_na()
+
+ggplot(df_n_sp, aes(x = n_sp_2025, y = n_sp_2020, color = Site)) +
+  geom_jitter(size = 3,width = 0.2, height = 0.2) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red", linewidth = 1) +
+  coord_fixed(ratio = 1, xlim = c(0, 10), ylim = c(0, 10), expand = TRUE) +
+  labs(
+    title = "Nombre d'espèces par site",
+    x = "2025",
+    y = "2020",
+    color = "Site"
+  ) +
+  theme_minimal()+
+  theme(
+    panel.grid.major = element_line(color = "grey60", size = 0.6),  # Quadrillage principal renforcé
+    panel.grid.minor = element_line(color = "grey80", size = 0.3),  # Quadrillage secondaire visible
+    panel.border = element_rect(color = "black", fill = NA, size = 0.8),  # Contour du graphique
+    axis.line = element_line(color = "black", size = 0.8)  # Axes visibles
+  )
+
+##AFC ####
+
+#Transformation des datas avec Hellinger
+data_afc = Macro_Ptscontacts[, -c(19, 22)]
+data_afc_clean = data_afc %>% mutate(across(everything(), ~replace_na(.x, 0)))
+meta = data_afc_clean %>% select(annee, Site, LAGUNE)
+data_num = data_afc_clean %>% select(-annee, -Site, -LAGUNE)
+data_hellinger = decostand(data_num, method = "hellinger")
+
+#Faire l'ACP sur données transformées
+pca_res = PCA(data_hellinger, graph = FALSE)
+
+#Extraction des coordonnées meta
+coord_ind = as.data.frame(pca_res$ind$coord[, 1:2])
+colnames(coord_ind) = c("Dim.1", "Dim.2")
+coord_ind = cbind(meta, coord_ind)
+
+# Extraction coordonnées variables (espèces)
+coord_var = as.data.frame(pca_res$var$coord[, 1:2])
+colnames(coord_var) = c("Dim.1", "Dim.2")
+coord_var$Espece = rownames(coord_var)
+
+#### Graph avec polygone #### 
+
+get_hull = function(df) df[chull(df$Dim.1, df$Dim.2), ]
+hulls = coord_ind %>% group_by(Site) %>% group_modify(~get_hull(.x))
+
+ggplot(coord_ind, aes(x = Dim.1, y = Dim.2, color = Site)) +
+  geom_point(size = 3, alpha = 0.8) +
+  geom_polygon(data = hulls, aes(x = Dim.1, y = Dim.2, fill = Site), alpha = 0.15, color = NA, inherit.aes = FALSE) +
+  geom_segment(data = coord_var, aes(x = 0, y = 0, xend = Dim.1, yend = Dim.2),
+               arrow = arrow(length = unit(0.2, "cm")), color = "black", inherit.aes = FALSE) +
+  geom_text_repel(data = coord_var, aes(x = Dim.1, y = Dim.2, label = Espece),
+                  color = "black", size = 3, inherit.aes = FALSE, max.overlaps = 20) +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "grey40") +
+  geom_vline(xintercept = 0, linetype = "dotted", color = "grey40") +
+  labs(title = "ACP sur données transformées Hellinger\navec lagunes colorées par site et flèches espèces",
+       x = "Dimension 1",
+       y = "Dimension 2") +
+  theme_minimal() +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.line = element_line(color = "black")
+  )
+
+
+#Graph polygone mais par LAGUNE 
+
+group_circles = coord_ind %>%
+  group_by(LAGUNE) %>%
+  summarise(
+    x0 = mean(Dim.1),
+    y0 = mean(Dim.2),
+    r = max(sqrt((Dim.1 - mean(Dim.1))^2 + (Dim.2 - mean(Dim.2))^2)) + 0.1
+  )
+ggplot(coord_ind, aes(x = Dim.1, y = Dim.2)) +
+  geom_point(aes(color = LAGUNE), size = 3, alpha = 0.8) +
+  
+  geom_circle(data = group_circles, aes(x0 = x0, y0 = y0, r = r, fill = LAGUNE),
+              alpha = 0.15, color = NA, inherit.aes = FALSE) +
+  coord_fixed() +
+  theme_minimal()
+
+#Sortir les outliers et refaire le graph par site : PAL_VIL_06 et 07 
+
+ggplot(coord_ind %>% filter(!LAGUNE %in% c("PAL_VIL_07", "PAL_VIL_06","ORB_MAI_04","ORB_MAI_05")), aes(x = Dim.1, y = Dim.2, color = Site)) +
+  geom_point(size = 3, alpha = 0.8) +
+  geom_polygon(
+    data = (coord_ind %>% filter(!LAGUNE %in% c("PAL_VIL_07", "PAL_VIL_06","ORB_MAI_04","ORB_MAI_05")) %>% group_by(Site) %>% group_modify(~get_hull(.x))),
+    aes(x = Dim.1, y = Dim.2, fill = Site),
+    alpha = 0.15, color = NA, inherit.aes = FALSE
+  ) +
+  geom_segment(
+    data = coord_var, aes(x = 0, y = 0, xend = Dim.1, yend = Dim.2),
+    arrow = arrow(length = unit(0.2, "cm")), color = "black", inherit.aes = FALSE
+  ) +
+  geom_text_repel(
+    data = coord_var, aes(x = Dim.1, y = Dim.2, label = Espece),
+    color = "black", size = 6, inherit.aes = FALSE, max.overlaps = 20
+  ) +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "grey40") +
+  geom_vline(xintercept = 0, linetype = "dotted", color = "grey40") +
+  labs(
+    title = "ACP sur données transformées Hellinger\navec lagunes colorées par site et flèches espèces",
+    x = "Dimension 1",
+    y = "Dimension 2"
+  ) +
+  theme_minimal() +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA),
+    axis.line = element_line(color = "black"),
+    plot.title = element_text(size = 40, face = "bold"),       
+    axis.title = element_text(size = 30),                     
+    axis.text = element_text(size = 24),                      
+    legend.title = element_text(size = 28),                    
+    legend.text = element_text(size = 24),                     
+    strip.text = element_text(size = 28),                     
+  )
 
 #### EXPLO INTER-SITES ####
 #Existe-il une difference entre les sites ? 
@@ -300,34 +467,7 @@ for (esp in especes) {
   }
 }
 
-#graph seul n sp 
-df_n_sp = Cov_tot_moy %>%
-  select(Annee, Site, LAGUNE, n_sp) %>%
-  filter(Annee %in% c(2020, 2025)) %>%
-  pivot_wider(
-    names_from = Annee,
-    values_from = n_sp,
-    names_prefix = "n_sp_"
-  ) %>%
-  drop_na()
 
-ggplot(df_n_sp, aes(x = n_sp_2025, y = n_sp_2020, color = Site)) +
-  geom_jitter(size = 3,width = 0.2, height = 0.2) +
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "red", linewidth = 1) +
-  coord_fixed(ratio = 1, xlim = c(0, 10), ylim = c(0, 10), expand = TRUE) +
-  labs(
-    title = "Nombre d'espèces par site",
-    x = "2025",
-    y = "2020",
-    color = "Site"
-  ) +
-  theme_minimal()+
-  theme(
-    panel.grid.major = element_line(color = "grey60", size = 0.6),  # Quadrillage principal renforcé
-    panel.grid.minor = element_line(color = "grey80", size = 0.3),  # Quadrillage secondaire visible
-    panel.border = element_rect(color = "black", fill = NA, size = 0.8),  # Contour du graphique
-    axis.line = element_line(color = "black", size = 0.8)  # Axes visibles
-  )
 
 #### ANALYSE TRAJECTOIRE ####
 
@@ -379,7 +519,7 @@ scores_df_triche_filtre = scores_df_triche %>%
 
 #Graph1 : Ellipses par sites 
 
-species_scores <- as.data.frame(scores(nmds_triche, display = "species"))
+species_scores = as.data.frame(scores(nmds_triche, display = "species"))
 species_scores$Species <- rownames(species_scores)
 colnames(species_scores)[1:2] <- c("NMDS1", "NMDS2")
 
@@ -406,7 +546,7 @@ ggplot(scores_df_triche, aes(x = NMDS1, y = NMDS2, color = Site)) +
 
 #Graph2 : Ellipses par Annees 
 
-centres <- scores_df_triche_filtre %>%
+centres = scores_df_triche_filtre %>%
   filter(Annee %in% c(2020, 2025)) %>%
   group_by(Annee) %>%
   summarise(
@@ -414,7 +554,7 @@ centres <- scores_df_triche_filtre %>%
     centre_NMDS2 = mean(NMDS2)
   )
 
-fleche <- data.frame(
+fleche = data.frame(
   x = centres$centre_NMDS1[centres$Annee == 2020],
   y = centres$centre_NMDS2[centres$Annee == 2020],
   xend = centres$centre_NMDS1[centres$Annee == 2025],
@@ -453,7 +593,7 @@ ggplot(scores_df_triche_filtre, aes(x = NMDS1, y = NMDS2, color = Annee)) +
 
 #Graph 3 : Ellipses par Site/Annees + fleches 
 
-scores_df_triche_filtre <- scores_df_triche_filtre %>%
+scores_df_triche_filtre = scores_df_triche_filtre %>%
   mutate(Site_Annee = paste(Site, Annee, sep = "_"))
 
 
@@ -462,12 +602,12 @@ scores_df_triche_filtre %>% #Identifier les groupes avec < 3 points
   summarise(nb_points = n()) %>%
   filter(nb_points < 3)
 
-scores_df_triche_filtre_ellipse <- scores_df_triche_filtre %>% #Garder uniquement les groupes avec au moins 3 points
+scores_df_triche_filtre_ellipse = scores_df_triche_filtre %>% #Garder uniquement les groupes avec au moins 3 points
   group_by(Site_Annee) %>%
   filter(n() >= 3) %>%
   ungroup()
 
-groupes_sans_variance <- scores_df_triche_filtre_ellipse %>% # Identifier les groupes sans variance (points identiques)
+groupes_sans_variance = scores_df_triche_filtre_ellipse %>% # Identifier les groupes sans variance (points identiques)
   group_by(Site_Annee) %>%
   summarise(
     var_NMDS1 = var(NMDS1),
@@ -477,7 +617,7 @@ groupes_sans_variance <- scores_df_triche_filtre_ellipse %>% # Identifier les gr
   filter((is.na(var_NMDS1) | var_NMDS1 < 1e-6) & 
            (is.na(var_NMDS2) | var_NMDS2 < 1e-6))
 
-cercles_centres <- scores_df_triche_filtre_ellipse %>%  # Extraire les centres pour les groupes sans variance
+cercles_centres = scores_df_triche_filtre_ellipse %>%  # Extraire les centres pour les groupes sans variance
   semi_join(groupes_sans_variance, by = "Site_Annee") %>%
   group_by(Site, Annee, Site_Annee) %>%
   summarise(NMDS1 = mean(NMDS1), NMDS2 = mean(NMDS2), .groups = "drop")
@@ -485,7 +625,7 @@ cercles_centres <- scores_df_triche_filtre_ellipse %>%  # Extraire les centres p
 scores_df_triche_filtre_ellipse_var <- scores_df_triche_filtre_ellipse %>% # Garder uniquement les groupes AVEC variance pour les ellipses
   anti_join(groupes_sans_variance, by = "Site_Annee")
 
-centroids_triche <- scores_df_triche_filtre_ellipse %>% # Calculer les centroïdes pour les flèches
+centroids_triche = scores_df_triche_filtre_ellipse %>% # Calculer les centroïdes pour les flèches
   group_by(Site, Annee, Site_Annee) %>%
   summarise(
     NMDS1 = mean(NMDS1, na.rm = TRUE),
@@ -494,7 +634,7 @@ centroids_triche <- scores_df_triche_filtre_ellipse %>% # Calculer les centroïd
   ) %>%
   arrange(Site, Annee)
 
-arrows_df_triche <- centroids_triche %>%  # Calculer les flèches entre centroïdes
+arrows_df_triche = centroids_triche %>%  # Calculer les flèches entre centroïdes
   group_by(Site) %>%
   arrange(Annee) %>%
   mutate(
@@ -523,7 +663,11 @@ ggplot(scores_df_triche_filtre_ellipse, aes(x = NMDS1, y = NMDS2, color = Site))
   labs(title = "Trajectoire temporelle des sites dans l’espace NMDS entre 2020 et 2025 (flèches entre ellipses)")
 
 #Graph 4 : Trajectoires par lagunes 
-ggplot(scores_df_triche_filtre, aes(x = NMDS1, y = NMDS2)) +
+scores_df_triche_piece_eau = scores_df_triche_filtre %>%
+  mutate(LAGUNE = str_remove(LAGUNE, "^[0-9]{4}_"))
+
+ggplot(scores_df_triche_piece_eau, aes(x = NMDS1, y = NMDS2)) +
+  geom_jitter(size = 2)+
   geom_path(aes(group = LAGUNE), 
             arrow = arrow(type = "closed", length = unit(0.05, "inches")),
             color = "grey50", linewidth = 0.3) +
